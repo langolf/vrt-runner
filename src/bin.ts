a9d785213bae5d04ba0038b05fcb8d1d3ad0003b
Ivan Grekov <ivan.grekov@magiclab.co>
2020-10-08 18:53:17 +0100

    fix types

--- src/bin.ts
+++ src/bin.ts
@@ -7,13 +7,13 @@ import runVrt from './index';
 const cwd = (argv.cwd as string) || process.cwd();
 const output = (argv.output as string) || path.resolve(cwd, 'result');
 const teamcity = !!argv.teamcity;
-const threshold = argv.threshold as number;
-const includeAA = argv.threshold as boolean;
-const alpha = argv.threshold as number;
-const aaColor = argv.threshold as [number, number, number];
-const diffColor = argv.threshold as [number, number, number];
-const diffColorAlt = argv.threshold as [number, number, number];
-const diffMask = argv.threshold as boolean;
+const threshold = argv.threshold as undefined | number;
+const includeAA = argv.includeAA as undefined | boolean;
+const alpha = argv.alpha as undefined | number;
+const aaColor = argv.aaColor as undefined | [number, number, number];
+const diffColor = argv.diffColor as undefined | [number, number, number];
+const diffColorAlt = argv.diffColorAlt as undefined | [number, number, number];
+const diffMask = argv.diffMask as undefined | boolean;
 const options = {
     threshold,
     includeAA,
